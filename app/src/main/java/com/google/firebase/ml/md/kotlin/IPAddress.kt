589103cfc895b2f85cc6cbdbc779a4463b29429f
wattanachaiBoom <boom_sirisuk@hotmail.com>
2020-03-25 12:25:11 +0700

    save before fragment

--- app/src/main/java/com/google/firebase/ml/md/kotlin/IPAddress.kt
+++ app/src/main/java/com/google/firebase/ml/md/kotlin/IPAddress.kt
@@ -2,6 +2,6 @@ package com.google.firebase.ml.md.kotlin
 
 class IPAddress {
     companion object {
-        val ipAddress = "http://192.168.43.138:3000/"
+        val ipAddress = "http://192.168.11.1:3000/"
     }
 }
